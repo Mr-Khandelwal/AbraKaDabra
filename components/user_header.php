8591d40b76622761af137ffeefa5cbc9071805d7
Rajat khandelwal <7878rajatkhandelwal@gmail.com>
2023-04-22 14:38:09 +0530

    header commit 2

--- components/user_header.php
+++ components/user_header.php
@@ -15,7 +15,7 @@
 
    <section class="flex">
 
-      <a href="home.php" class="logo">Shopie<span>.</span></a>
+      <a href="home.php" class="logo">Aabra Ka Dabra<span>.</span></a>
 
       <nav class="navbar">
          <a href="home.php">home</a>
